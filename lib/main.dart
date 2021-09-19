af742636aa6cd85e86da55242aafd3d1ec3ccd9b
Myothra7777 <turtwigandmew@yahoo.com>
2021-09-19 00:49:14 -0700

    Completed register/login/logout flow with simple email+pass

--- lib/main.dart
+++ lib/main.dart
@@ -10,49 +10,22 @@ Future<void> main() async {
   runApp(MyApp());
 }
 
-//Sign in
-appSignIn() async {
-  //Sign in
-  try {
-    UserCredential userCredential = await FirebaseAuth.instance
-        .signInWithEmailAndPassword(
-            email: "barry.allen@example.com", password: "SuperSecretPassword!");
-  } on FirebaseAuthException catch (e) {
-    if (e.code == 'user-not-found') {
-      print('No user found for that email.');
-    } else if (e.code == 'wrong-password') {
-      print('Wrong password provided for that user.');
-    }
-  }
-}
+//Define auth (got errors when this was moved into app class)
+FirebaseAuth auth = FirebaseAuth.instance;
 
-//Sign out
-appSignOut() async {
-  await FirebaseAuth.instance.signOut();
-}
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
-  bool signedIn = false;
-  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
-
-  //Define auth
-  FirebaseAuth auth = FirebaseAuth.instance;
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Taskify',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
           primarySwatch: Colors.blue,
         ),
         home: FutureBuilder(
@@ -62,28 +35,12 @@ class MyApp extends StatelessWidget {
               print('You have an error! ${snapshot.error.toString()}');
               return const Text('Something went wrong!');
             } else if (snapshot.hasData) {
-              //Check for state change
-              FirebaseAuth.instance.authStateChanges().listen((User? user) {
-                if (user == null) {
-                  print('User is currently signed out!');
-                  signedIn = false;
-                } else {
-                  print('User is signed in!');
-                  signedIn = true;
-                }
-              });
-              if (signedIn == true) {
-                return const MyHomePage(title: 'Taskify');
-              } else {
-                //return sign up / sign in page
-                return const Page2();
-              }
+              return Page2();
             } else {
               return const Center(
                 child: CircularProgressIndicator(),
               );
             }
-            //const MyHomePage(title: 'Flutter Demo Home Page'),
           },
         ));
   }
@@ -91,16 +48,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -110,53 +57,33 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  appSignOut() async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Page2()),
+    );
+  }
+
   void _incrementCounter() {
     DatabaseReference _testRef =
         FirebaseDatabase.instance.reference().child("test");
     _testRef.set("Hello world ${Random().nextInt(100)}");
 
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        automaticallyImplyLeading: false,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -166,14 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Page2()),
-                  );
-                },
-                child: const Text('Register'))
+            ElevatedButton(onPressed: appSignOut, child: const Text('Sign Out'))
           ],
         ),
       ),
@@ -181,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
@@ -189,22 +109,11 @@ class _MyHomePageState extends State<MyHomePage> {
 class Page2 extends StatefulWidget {
   const Page2({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
   State<Page2> createState() => _Page2State();
 }
 
 class _Page2State extends State<Page2> {
-  String email = '';
-  String password = '';
   final myController = TextEditingController();
   final myController2 = TextEditingController();
   appRegister(String uEmail, String uPass) async {
@@ -220,13 +129,52 @@ class _Page2State extends State<Page2> {
     } catch (e) {
       print(e);
     }
+    auth.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => const MyHomePage(title: 'Taskify')),
+        );
+      }
+    });
+  }
+
+  appSignIn(String uEmail, String uPass) async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance //Is this necessary???
+              .signInWithEmailAndPassword(email: uEmail, password: uPass);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+    auth.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => const MyHomePage(title: 'Taskify')),
+        );
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register'),
+        title: const Text('Login / Sign Up'),
+        automaticallyImplyLeading: false,
       ),
       body: Center(
         child: Column(
@@ -234,18 +182,20 @@ class _Page2State extends State<Page2> {
           children: <Widget>[
             TextField(
               decoration: const InputDecoration(labelText: 'Enter Email'),
-              //onChanged: (email),
               controller: myController,
             ),
             TextField(
               decoration: const InputDecoration(labelText: 'Enter Password'),
-              //onChanged: (email),
               controller: myController2,
             ),
             ElevatedButton(
                 onPressed: () =>
                     appRegister(myController.text, myController2.text),
-                child: const Text('Register'))
+                child: const Text('Register')),
+            ElevatedButton(
+                onPressed: () =>
+                    appSignIn(myController.text, myController2.text),
+                child: const Text('Sign In'))
           ],
         ),
       ),
